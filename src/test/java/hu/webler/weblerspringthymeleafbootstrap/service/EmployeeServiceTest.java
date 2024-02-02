64ad8b96469d9ec158b8f42d83ba51191024d4c8
gyepike <antipet@gmail.com>
2024-02-02 19:50:01 +0100

    Added Unit Test Without Mock

--- src/test/java/hu/webler/weblerspringthymeleafbootstrap/service/EmployeeServiceTest.java
+++ src/test/java/hu/webler/weblerspringthymeleafbootstrap/service/EmployeeServiceTest.java
@@ -44,8 +44,6 @@ public class EmployeeServiceTest {
         // Act
         Paged<Employee> employees = loaderLoader.getEmployees(0);
 
-        // Tested if Data present
-        // System.out.println("Total Elements: " + employees.getContents());
 
         // Assert
         assertNotNull(employees);
@@ -62,26 +60,23 @@ public class EmployeeServiceTest {
     void testGetEmployees_shouldReturnNonEmptyListOfEmployeesWithOutMock() { // naming convention: testMethodName_StateUnderTest_ExpectedBehavior instead of this: void testGetStudents()
 
         // Arrange
-        Paged<Employee> mockEmployees = createTestList();
-
-
-        when(loaderLoader.getEmployees(0)).thenReturn(mockEmployees);
+        Employee emp1 = new Employee("Employee 2", "Position 2", "Office 2", null, 51000.0);
 
         // Act
         Paged<Employee> employees = loaderLoader.getEmployees(0);
 
-        // Tested if Data present
-        // System.out.println("Total Elements: " + employees.getContents());
 
         // Assert
         assertNotNull(employees);
-        assertEquals(2, employees.getContents().size());
+        assertEquals(10, employees.getContents().size());
 
-        Employee emp1 = new Employee("Gyepi", "Position1", "Bp", null, 1200);
-        assertEquals(emp1, employees.getContents().get(1));
+
+        Employee emp2 = employees.getContents().get(1);
+        emp2.setStartDate(null);
+
+        assertEquals(emp1, emp2);
 
     }
-
 
 
     private Paged<Employee> createTestList() {
